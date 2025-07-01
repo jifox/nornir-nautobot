a2570709650e36236841910236bcb9a6c1738580
Josef Fuchs <josef.fuchs@pankl.com>
2025-07-01 14:13:10 +0200

    change class names to match dispatchers common naming

--- nornir_nautobot/plugins/tasks/dispatcher/hp_comware.py
+++ nornir_nautobot/plugins/tasks/dispatcher/hp_comware.py
@@ -1,9 +1,13 @@
 """nornir dispatcher for hp_comware devices."""
 
-from nornir_nautobot.plugins.tasks.dispatcher.default import NetmikoDefault
+from nornir_nautobot.plugins.tasks.dispatcher.default import NapalmDefault, NetmikoDefault
 
 
-class HPComwareDefault(NetmikoDefault):
+class NapalmHPComware(NetmikoDefault):
+    """Collection of Napalm Nornir Tasks specific to HPE Comware 5/7 devices."""
+
+
+class NetmikoHPComware(NetmikoDefault):
     """Collection of Netmiko Nornir Tasks specific to HPE Comware 5/7 devices."""
 
     config_command = "display current-configuration"
--- nornir_nautobot/plugins/tasks/dispatcher/hp_procurve.py
+++ nornir_nautobot/plugins/tasks/dispatcher/hp_procurve.py
@@ -1,9 +1,13 @@
 """nornir dispatcher for hp_procurve devices."""
 
-from nornir_nautobot.plugins.tasks.dispatcher.default import NetmikoDefault
+from nornir_nautobot.plugins.tasks.dispatcher.default import NapalmDefault, NetmikoDefault
 
 
-class HPProcurveDefault(NetmikoDefault):
+class NapalmHPProcurve(NapalmDefault):
+    """Collection of Naplam Nornir Tasks specific to HP Procurve devices."""
+
+
+class NetmikoHPProcurve(NetmikoDefault):
     """Collection of Netmiko Nornir Tasks specific to HP Procurve devices."""
 
     config_command = "show running-config"
